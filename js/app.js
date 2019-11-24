2e838d8ce239047a9bc1358da3ef76095451dde2
Abiodun Sukurat <abiodunafolasade@yahoo.com>
2019-11-24 05:52:28 +0100

    bootstrap project

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const typed = new Typed('#intro-typed', {
+    strings: ['more professional', 'pefect in every way', 'astonishing'],
+    typeSpeed: 40,
+    backSpeed: 40,
+    startDelay: 1000,
+    loop: true
+
+})
